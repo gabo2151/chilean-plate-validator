a55c075a99cc733086024d64645962c17d4d5e99
gabo2151 <gabo.2151@hotmail.com>
2022-07-02 23:51:27 -0400

    Jest integration for Unit Testing

---
+++ tests/index.test.ts
@@ -0,0 +1,46 @@
+import { plateValid, plateType, CLPlate } from '../src';
+
+const tests = [
+  { plate: '', plateValid: false, plateType: 'INVALID' },
+  { plate: 'BBCC10', plateValid: true, plateType: 'NEW_VEHICLE_PLATE' },
+  { plate: 'AAXX00', plateValid: false, plateType: 'INVALID' },
+  { plate: 'BBCC1', plateValid: false, plateType: 'INVALID' },
+  { plate: 'BBBCC10', plateValid: false, plateType: 'INVALID' },
+  { plate: 'BBCC00', plateValid: false, plateType: 'INVALID' },
+  { plate: 'BB10CC', plateValid: false, plateType: 'INVALID' },
+  { plate: 'SEXY69', plateValid: false, plateType: 'INVALID' },
+  { plate: 'JJCC27', plateValid: true, plateType: 'NEW_VEHICLE_PLATE' },
+  { plate: 'AR1240', plateValid: true, plateType: 'OLD_PLATE' },
+  { plate: 'CBC320', plateValid: true, plateType: 'NEW_MOTORCYCLE_PLATE' },
+  { plate: 'AC20', plateValid: false, plateType: 'INVALID' },
+  { plate: 'ACX200', plateValid: false, plateType: 'INVALID' },
+  { plate: 'BBBB2000', plateValid: false, plateType: 'INVALID' },
+];
+
+describe('testing index file', () => {
+
+  tests.forEach(_test => {
+    test(`\'${_test.plate}\' should result in \'${_test.plateValid}\'`, () => {
+      expect(plateValid(_test.plate)).toBe(_test.plateValid);
+    })
+  });
+
+  tests.forEach(_test => {
+    test(`\'${_test.plate}\' should result in \'${_test.plateType}\'`, () => {
+      expect(plateType(_test.plate)).toBe(_test.plateType);
+    })
+  });
+
+  tests.forEach(_test => {
+    test(`new CLPlate(\'${_test.plate}\').valid should result in \'${_test.plateValid}\'`, () => {
+      expect(new CLPlate(_test.plate).valid).toBe(_test.plateValid);
+    })
+  });
+
+  tests.forEach(_test => {
+    test(`new CLPlate(\'${_test.plate}\').type should result in \'${_test.plateType}\'`, () => {
+      expect(new CLPlate(_test.plate).type).toBe(_test.plateType);
+    })
+  });
+
+});
