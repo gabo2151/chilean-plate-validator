b099cd6582045c80c17e1f8ce3c45fba127684b9
gabo2151 <gabo.2151@hotmail.com>
2022-07-03 15:33:18 -0400

    Test configuration fixes

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@ module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  testResultsProcessor: 'jest-sonar-reporter',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/test/'
+  ]
 }
